100f57fb526344d8ddbabae0bcd18142bbaf1e37
pavelv <pavelv87@gmail.com>
2021-05-19 16:07:39 +0600

    fix changes

--- knexfile.js
+++ knexfile.js
@@ -24,6 +24,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`,
+    ssl: { rejectUnauthorized: false },
     // client: 'sqlite3',
     // connection: {
     //   filename: './database.sqlite',
