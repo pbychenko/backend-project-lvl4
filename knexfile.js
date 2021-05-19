516b82fa8412aa5494005298abdc2933e4c0d5ac
pavel <pavelv87@gmail.com>
2021-05-19 20:28:35 +0600

    try to test

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
     useNullAsDefault: true,
     migrations,
   },
